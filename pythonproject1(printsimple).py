78038a5b0c2d080b8d8532606b1ad63945596ff6
KrazyDevPixel <arham.sjws25@gmail.com>
2026-09-10 17:58:46 +0600

    Add files via upload

---
+++ pythonproject1(printsimple).py
@@ -0,0 +1,5 @@
+#Print a greeting message and 2 sentences about yourself with a simple equation
+print("Hello World!")
+print("My name is Arham")
+print("I am 10 years old")
+print(f"The sum of 1+42 is {1+42}")
